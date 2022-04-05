82f8c97943c794bbda5a1da0a5d79393278fcb24
FazleRahmanEvan <evanshakib26@gmail.com>
2022-04-05 18:56:08 +0600

    about commit

--- src/componenets/About/About.js
+++ src/componenets/About/About.js
@@ -4,7 +4,9 @@ const About = () => {
     return (
        
         <div className="font-bold text-lg mt-[100px]">
-       
+        <h2 className="flex justify-center">I am Md. Fazle Rahman Evan</h2>
+        <h3 className="flex justify-center">I am Learning Web Programming</h3>
+        <p className="flex justify-center">Live in Sylhet,Bangladesh</p>
       </div>
       
     );
